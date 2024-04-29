0cb5c3019a32fd76a7ca556957b93b4441dd63c9
xm <874738758@qq.com>
2024-04-29 15:39:39 +0000

    fix auth

--- src/middleware/basicAuth.js
+++ src/middleware/basicAuth.js
@@ -2,11 +2,11 @@
  * When applied, this middleware will ensure the request contains the required header for basic authentication and only
  * allow access to the endpoint after successful authentication.
  */
-const { getConfig } = require('../util.js');
+const { getConfig } = require("../util.js");
 
 const unauthorizedResponse = (res) => {
-    res.set('WWW-Authenticate', 'Basic realm="SillyTavern", charset="UTF-8"');
-    return res.status(401).send('Authentication required');
+    res.set("WWW-Authenticate", 'Basic realm="SillyTavern", charset="UTF-8"');
+    return res.status(401).send("Authentication required");
 };
 
 const basicAuthMiddleware = function (request, response, callback) {
@@ -17,17 +17,20 @@ const basicAuthMiddleware = function (request, response, callback) {
         return unauthorizedResponse(response);
     }
 
-    const [scheme, credentials] = authHeader.split(' ');
+    const [scheme, credentials] = authHeader.split(" ");
 
-    if (scheme !== 'Basic' || !credentials) {
+    if (scheme !== "Basic" || !credentials) {
         return unauthorizedResponse(response);
     }
 
-    const [username, password] = Buffer.from(credentials, 'base64')
-        .toString('utf8')
-        .split(':');
+    const [username, password] = Buffer.from(credentials, "base64")
+        .toString("utf8")
+        .split(":");
 
-    if (username === process.env.username || config.basicAuthUser.username && password === process.env.password || config.basicAuthUser.password) { //if (username === config.basicAuthUser.username && password === config.basicAuthUser.password) {
+    if (
+        username === config.basicAuthUser.username &&
+        password === config.basicAuthUser.password
+    ) {
         return callback();
     } else {
         return unauthorizedResponse(response);
